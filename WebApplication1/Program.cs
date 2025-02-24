8c7638798cff5c35d0f5007b9b879047a586c339
John Gibson <johnmgibson3@gmail.com>
2025-02-24 12:57:28 -0700

    changes

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,10 +1,29 @@
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
-
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-// Serving static files change app.MapGet
-// app.MapGet("/", () => "Hello World!");
-app.UseDefaultFiles();
-app.UseStaticFiles();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
 app.Run();
